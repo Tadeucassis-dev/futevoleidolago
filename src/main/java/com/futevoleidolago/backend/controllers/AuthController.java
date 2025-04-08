c920230151233432840c732f45dccfd04def9d48
tadeu.cesar <tadeu.cesar@worldconnectoficial.com.br>
2025-04-08 13:59:20 -0300

    implementando login

--- src/main/java/com/futevoleidolago/backend/controllers/AuthController.java
+++ src/main/java/com/futevoleidolago/backend/controllers/AuthController.java
@@ -1,9 +1,12 @@
 package com.futevoleidolago.backend.controllers;
 
+import com.futevoleidolago.backend.dto.LoginRequestDTO;
+import com.futevoleidolago.backend.repositories.UserRepository;
 import com.futevoleidolago.backend.service.AuthService;
 import com.futevoleidolago.backend.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -11,16 +14,24 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api")
 public class AuthController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Autowired
     private AuthService authService;
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody Map<String, String> credentials) {
-        String token = authService.login(
-                credentials.get("email"),
-                credentials.get("password")
-        );
-        return ResponseEntity.ok(Map.of("token", token));
+    public ResponseEntity<Map<String, String>> login(@RequestBody LoginRequestDTO credentials) {
+        try {
+            String token = authService.login(credentials.getEmail(), credentials.getPassword());
+            return ResponseEntity.ok(Map.of("token", token));
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(401).body(Map.of("error", e.getMessage()));
+        }
     }
 
     @PostMapping("/register")
